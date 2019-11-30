f0379ab1161b6cfc4ef4ebcf004f3a4de3763533
Elvinas Lukasevicius <ell@open.ch>
2019-11-30 16:29:35 +0100

    Unit tests added

--- src/test/java/OrderControllerTest.java
+++ src/test/java/OrderControllerTest.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +17,14 @@ import webshop.resources.Order;
 
 import java.math.BigDecimal;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringJUnitWebConfig(classes = WebConfig.class)
 public class OrderControllerTest {
 
     @Autowired
     private WebApplicationContext wac;
-
+    private ObjectMapper mapper = new ObjectMapper();
     private MockMvc mockMvc;
 
     @BeforeEach
@@ -36,19 +34,71 @@ public class OrderControllerTest {
 
     @Test
     public void testAdd() throws Exception {
+
+
+        MvcResult mvcResult = this.mockMvc.perform(post("/orders/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.getJsonOrder()))
+                .andReturn();
+
+        Assertions.assertEquals(mvcResult.getResponse().getContentAsString(), "{\"id\":1,\"amount\":10,\"name\":\"Test 1\"}");
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        // NOTE: This test will only work if we test add order before!
+        MvcResult mvcResult = this.mockMvc.perform(get("/orders/all"))
+                .andReturn();
+
+        Assertions.assertEquals(mvcResult.getResponse().getContentAsString(), "[{\"id\":1,\"amount\":10,\"name\":\"Test 1\"}]");
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        // NOTE: This test will only work if we test add order before!
         Order order = new Order();
-        order.setName("Test 1");
-        order.setAmount(BigDecimal.TEN);
+        order.setId(1L);
+        order.setName("Test 2");
+        order.setAmount(BigDecimal.ONE);
 
         ObjectMapper mapper = new ObjectMapper();
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
         String requestJson = ow.writeValueAsString(order);
 
-        MvcResult mvcResult = this.mockMvc.perform(post("/orders/add")
+        MvcResult mvcResult = this.mockMvc.perform(put("/orders/update")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestJson))
                 .andReturn();
 
-        Assertions.assertEquals(mvcResult.getResponse().getContentAsString(), "{\"id\":1,\"amount\":10,\"name\":\"Test 1\"}");
+        Assertions.assertEquals(mvcResult.getResponse().getContentAsString(), "{\"id\":1,\"amount\":1,\"name\":\"Test 2\"}");
     }
+
+    @Test
+    public void testDelete() throws Exception {
+        // NOTE: This test will only work if we test add order before!
+
+        MvcResult mvcResultCreated = this.mockMvc.perform(post("/orders/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.getJsonOrder()))
+                .andReturn();
+
+
+        Order orderToDelete = mapper.readValue(mvcResultCreated.getResponse().getContentAsString(), Order.class);
+
+        MvcResult mvcResultDelete = this.mockMvc.perform(delete("/orders/delete?id=" + orderToDelete.getId()))
+                .andReturn();
+
+        Assertions.assertEquals(mvcResultDelete.getResponse().getContentAsString(), "[{\"id\":1,\"amount\":10,\"name\":\"Test 1\"}]");
+    }
+
+    private String getJsonOrder() throws JsonProcessingException {
+        Order order = new Order();
+        order.setName("Test 1");
+        order.setAmount(BigDecimal.TEN);
+
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(order);
+
+    }
+
 }
